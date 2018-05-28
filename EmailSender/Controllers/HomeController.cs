14a8e7761cb797b5eaa71583b7a16e45151c2bec
Sageer Shahzad <contact@shazplus.co.uk>
2018-05-28 03:27:58 +0100

    EmaiSenderProject

---
+++ EmailSender/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using EmailSender.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace EmailSender.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult RegisterationPage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult RegisterationPage(Registeration reg)
+        {
+            if (ModelState.IsValid)
+            {
+                WebMail.Send("yourEmail", reg.Name + "is coming to interview", reg.Message + "<br/><br/> My Number: " + reg.Mobile + "<br/><br/>My Email: " + reg.Email, null, null, null, true, null, null, null, null, null, reg.Email);
+            }
+            return RedirectToAction("ThankYou");
+        }
+
+        public ActionResult ThankYou()
+        {
+            return View();
+        }
+    }
+}
---
+++ EmailSender/Models/Registeration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EmailSender.Models
+{
+    public class Registeration
+    {
+        [Required(ErrorMessage = "Name is Required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is Required")]
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$", ErrorMessage = "Email Format is incorrect")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Mobile is Required")]
+        public int Mobile { get; set; }
+
+        [Required(ErrorMessage = "Message is Required")]
+        public string Message { get; set; }
+    }
+}
